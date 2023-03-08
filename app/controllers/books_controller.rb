4769fe8a394e553e91a77e923800c43d22631973
EC2 Default User <ec2-user@ip-172-31-19-177.ap-northeast-1.compute.internal>
2023-03-08 15:04:33 +0000

    books1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,51 +1,55 @@
 class BooksController < ApplicationController
-    def top
-    end
     
-    def index
-        @list = Book.new
-        @lists = Book.all
-    end
-    
-    def show
-        @lists = Book.find(params[:id])
-    end
-    
-    def create
-        list = Book.new(book_params)
-        if list.save
-            flash[:notice] = "Book was successfully created."
-            redirect_to"/books/#{list.id}"
-        else
-            flash[:alrat] = "投稿失敗"
-        end
-    end
-    
-    def edit
-        @lists = Book.find(params[:id])
-    end
-    
-    def update
-        @lists = Book.find(params[:id])
-        if @lists.update(book_params)
-            flash[:notice] = "Book was successfully update."
-            redirect_to"/books/#{@lists.id}"
-        else
-            flash[:alrat] = "投稿失敗"
+        def top
         end
         
-    end
+        def index
+            @lists = Book.all
+            @list = Book.new
+        end
     
-    def destroy
-        @lists = Book.find(params[:id])
-        @lists.destroy
-        redirect_to'/books'
-    end
+        def show
+            @lists = Book.find(params[:id])
+        end
+        
+        def create
+            @lists = Book.new(book_params)
+            if @lists.save
+                flash[:notice] = "Book was successfully created."
+                redirect_to"/books/#{@lists.id}"
+            else
+                flash[:alrat] = "2 errors prohibited this book from being saved:"
+                redirect_to"/books"
+            end
+        end
     
-    private
+        def edit
+            @lists = Book.find(params[:id])
+        end
     
-    def book_params
-    params.require(:book).permit(:title, :body)
-    
-    end
+        def update
+            @lists = Book.find(params[:id])
+            if @lists.update(book_params)
+                flash[:notice] = "Book was successfully update."
+                redirect_to"/books/#{@lists.id}"
+            else
+                flash[:alrat] = "1 errors prohibited this book from being saved:"
+                render "edit"
+            end
+            
+        end
+        
+        def destroy
+            @lists = Book.find(params[:id])
+            @lists.destroy
+            redirect_to'/books'
+        end
+        
+        private
+        
+        def book_params
+        params.require(:book).permit(:title, :body)
+        
+        end
+        
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get'/' => 'books#top'
-  get 'books/new'
-  get '/books' => 'books#index'
-  post '/books' => 'books#create'
-  get '/books/:id' => 'books#show',as: 'book'
-  get '/books/:id/edit' => 'books#edit',as: 'edit_list'
-  patch '/books/:id' => 'books#update',as: 'update_list'
-  delete '/books/:id' => 'books#destroy', as: 'destroy_list'
-  
+  # get 'books/new'
+  # get '/books' => 'books#index'
+  # post '/books' => 'books#create'
+  # get '/books/:id' => 'books#show',as: 'book'
+  # get '/books/:id/edit' => 'books#edit',as: 'edit_list'
+  # patch '/books/:id' => 'books#update',as: 'update_list'
+  # delete '/books/:id' => 'books#destroy', as: 'destroy_list'
+  resources :books
 end
